85ec8088327e57d2b0a4e829ae37547a34991ddb
hallin-dev <h41lin.business@gmail.com>
2025-08-21 16:21:01 +0900

    Update server.js

--- server.js
+++ server.js
@@ -1,12 +1,14 @@
 const express = require('express');
-const app = express();
 const cors = require('cors');
+const app = express();
+
 app.use(cors());
-app.use(express.json());
 
 app.get('/api/hello', (req, res) => {
-  res.json({ message: '작동 돼누' });
+  res.json({ message: 'possible' });
 });
 
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+  console.log(`Server listening on port ${PORT}`);
+});
